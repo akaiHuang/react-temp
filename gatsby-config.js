ed36d68d0aefe78774b6ee682380dd79598e7d76
akai <akai@fawstudio.com>
2020-04-28 01:53:23 +0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter + FAWstudio temp`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@akaihuang`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
